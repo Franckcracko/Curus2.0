e13f009aeb669b9dab89d026b647955c9c29ef7e
Franckcracko <brianleonardomtzcoboswigueta@gmail.com>
2023-05-28 11:55:49 -0500

    modified nodemailer.js change status code

--- pages/api/nodemailer.js
+++ pages/api/nodemailer.js
@@ -7,7 +7,7 @@ export default async function ContactAPI(req, res) {
     const resFirebase = await firebaseReq({data:req.body,id})
     if (!resFirebase) {
         console.log("Ocurrio un error en la llamada de la base de datos")
-        return res.status(500).json({ message: "error" });
+        return res.status(500).json({ message: "error database" });
     }
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
@@ -39,6 +39,6 @@ export default async function ContactAPI(req, res) {
         return res.status(200).json({ message: "succes" });
     } catch (error) {
         console.log("nodemailer");
-        return res.status(500).json({ message: "error" });
+        return res.status(500).json({ message: "error service mailer" });
     }
 }
